941293d4059e6c11bbdeb1246ba5569f7c0f44d5
Roman Slobolinsky <rslobolinsky13@gmail.com>
2023-12-26 17:08:16 +0400

    Create class Operations.py

---
+++ src/Operations.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self):
+        self.list_operation = []
+
+    def __repr__(self):
+        return (f"Return file with information\n"
+                f"about users operations:\n"
+                f"{self.list_operations}\n")
