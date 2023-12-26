02298f0b8f96eb02db229d343240c858b2b879da
Roman Slobolinsky <rslobolinsky13@gmail.com>
2023-12-26 17:25:26 +0400

    add test_print_first_line

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,6 +5,7 @@ from src.Operations import Operation
 def test_get_operations_list():
     assert Operation().get_operations_list(DATA_TEST) == [1, 2, 3]
 
+
 def test_clean_operations_list():
     operations = Operation()
     operations.get_operations_list(DATA_TEST)
@@ -13,3 +14,15 @@ def test_clean_operations_list():
     assert Operation().clean_operations_list([{'state': 'EXECUTED'}, {}, {'state': 'CANCELED'}]) == [
         {'state': 'EXECUTED'}]
     assert len(operations.list_operation) == 3
+
+
+def test_sort_list():
+    assert Operation().sort_list([{'date': '2018-01-21T01:10:28.317704'}, {'date': '2018-12-18T17:07:09.800800'},
+                                  {'date': '2019-12-08T22:46:21.935582'}]) == [
+               {'date': '2019-12-08T22:46:21.935582'},
+               {'date': '2018-12-18T17:07:09.800800'},
+               {'date': '2018-01-21T01:10:28.317704'}]
+
+
+def test_print_first_line():
+    assert Operation().print_first_line("2019-12-08T22:46:21.935582", "Открытие вклада") == "08.12.2019 Открытие вклада"
