8571d9e5a2577ebca5d4b7a0c1df980ef3d3fcc8
Roman Slobolinsky <rslobolinsky13@gmail.com>
2023-12-26 17:18:32 +0400

    add test_clean_operations_list

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,3 +4,12 @@ from src.Operations import Operation
 
 def test_get_operations_list():
     assert Operation().get_operations_list(DATA_TEST) == [1, 2, 3]
+
+def test_clean_operations_list():
+    operations = Operation()
+    operations.get_operations_list(DATA_TEST)
+    assert Operation().clean_operations_list([{'state': 'EXECUTED'}, {}, {'state': 'CANCELED'}]) == [
+        {'state': 'EXECUTED'}]
+    assert Operation().clean_operations_list([{'state': 'EXECUTED'}, {}, {'state': 'CANCELED'}]) == [
+        {'state': 'EXECUTED'}]
+    assert len(operations.list_operation) == 3
