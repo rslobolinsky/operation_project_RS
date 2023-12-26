09e905ffb9bc5dde8c7c50e3575e2f19811c6e92
Roman Slobolinsky <rslobolinsky13@gmail.com>
2023-12-26 17:10:07 +0400

    add get_operations_list

--- src/Operations.py
+++ src/Operations.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Operation:
     def __init__(self):
         self.list_operation = []
@@ -6,3 +9,13 @@ class Operation:
         return (f"Return file with information\n"
                 f"about users operations:\n"
                 f"{self.list_operations}\n")
+
+    def get_operations_list(self, data_path):
+        """
+        Get list with operations from data file
+        :param data_path: path to data file
+        :return: list with user operations
+        """
+        with open(data_path, 'r', encoding='UTF-8') as data_file:
+            self.list_operation = json.load(data_file)
+        return self.list_operation
